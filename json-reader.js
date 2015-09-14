d222dbb9a93f30427dbd707f2bb467beae3aee18
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-09-13 23:53:42 -0400

    added a callback aspect to this algirithm

--- json-reader.js
+++ json-reader.js
@@ -26,7 +26,7 @@ var array = "/object/object/object/object/object".split("/");
 array.shift()
 
 // this function reads into a given object for a depth of a given distence
-var findWithin = function(obj, arr, num) {
+var findWithin = function(obj, arr, num, cb) {
 	// this assigns a reference variable to the given object
 	var data = obj;
 	// this assigns a variable to the given array
@@ -38,14 +38,20 @@ var findWithin = function(obj, arr, num) {
 		// this essentially steps deeper into the object referenced and assigns 'data' to it
 		data = data[arr[i]] || {};
 	}
-	// this adds a key 'finish' with a value of 'true' to our desired object
-	data.finish = true;
+	// this this uses are callback provided.
+	// here we pass 'data' as our argument to it so that it can modify the data as needed
+	cb(data);
 	// returns the object that data is inside
 	return data;
 }
+
+// this is our callback to modify data within
+var callback = function(data) {
+	data.finish = true;
+}
 // calls the function with the necessary arguments
 // assigns a variable to our returned data
-var returnedData = findWithin(object, array, 5);
+var returnedData = findWithin(object, array, 5, callback);
 
 // console logs our data for visibility
 console.log("-----------------array-------------------: \n", array);
